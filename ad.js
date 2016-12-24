20c7e54d352e1f2583aaf4cf98d22366c7896859
lvjinqiao <365033268@qq.com>
2016-12-24 22:51:51 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -12,7 +12,7 @@
 
 */
 
-function FindProxyForURL(url, host){
+function FindUserProxyForURL(url, host){
 
     /********************************************
     *                                           *
@@ -24,7 +24,7 @@ function FindProxyForURL(url, host){
     *                                           *
     ********************************************/
 
-    var mode = 0;
+    var mode = 2;
 
     //【以下http代理设置仅在 “mode = 1” 时有效】
     var domain = "127.0.0.1";
